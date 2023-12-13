30f0560788e64fdb913b76ce88a45b23e492ceae
Max Ast <maxast31@gmail.com>
2023-12-13 18:09:13 +0100

    add firebase example

---
+++ examples/with-firebase/App.tsx
@@ -0,0 +1,98 @@
+import auth, { type FirebaseAuthTypes } from "@react-native-firebase/auth";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback, useEffect, useState } from "react";
+import { Alert, Button, StyleSheet, Text, View } from "react-native";
+
+SplashScreen.preventAutoHideAsync();
+
+export default function App() {
+  const [session, setSession] = useState<FirebaseAuthTypes.User | null>(null);
+  const [fontsLoaded, fontError] = useFonts({
+    "Inter-Black": require("./assets/fonts/Inter-Black.otf"),
+  });
+
+  useEffect(() => {
+    auth()
+      .useUserAccessGroup("group.expo.modules.shareextension.withfirebase")
+      .then(() => {
+        console.log("set up user access group on main app");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  useEffect(() => {
+    const unsubscribe = auth().onAuthStateChanged(async (user) => {
+      console.log("I ran on main app");
+      if (user) {
+        try {
+          setSession(user);
+        } catch (error) {
+          console.log(error);
+        }
+      } else {
+        setSession(null);
+      }
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, [setSession]);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
+  return (
+    <View onLayout={onLayoutRootView} style={styles.container}>
+      <Text style={{ color: "#313639", fontFamily: "Inter-Black" }}>
+        Firebase Demo
+      </Text>
+      {session ? (
+        <View>
+          <Text style={{ color: "#313639", fontFamily: "Inter-Black" }}>
+            Firebase User ID: {session.uid}
+          </Text>
+          <Button
+            title="Sign Out"
+            onPress={() =>
+              auth()
+                .signOut()
+                .catch((error) =>
+                  Alert.alert("Authentication Error", error.message)
+                )
+            }
+          />
+        </View>
+      ) : (
+        <Button
+          title="Sign In"
+          onPress={() =>
+            auth()
+              .signInAnonymously()
+              .catch((error) =>
+                Alert.alert("Authentication Error", error.message)
+              )
+          }
+        />
+      )}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FAF8F5",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
---
+++ examples/with-firebase/ShareExtension.tsx
@@ -0,0 +1,82 @@
+import auth, { type FirebaseAuthTypes } from "@react-native-firebase/auth";
+import { useEffect, useState } from "react";
+import { Alert, Button, Text, View, StyleSheet } from "react-native";
+
+export default function ShareExtension({ url }: { url: string }) {
+  const [session, setSession] = useState<FirebaseAuthTypes.User | null>(null);
+
+  useEffect(() => {
+    auth()
+      .useUserAccessGroup("group.expo.modules.shareextension.withfirebase")
+      .then(() => {
+        console.log("set up user access group on share extension");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  useEffect(() => {
+    const unsubscribe = auth().onAuthStateChanged(async (user) => {
+      console.log("I ran on share extension");
+      if (user) {
+        try {
+          setSession(user);
+        } catch (error) {
+          console.log(error);
+        }
+      } else {
+        setSession(null);
+      }
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, [setSession]);
+
+  return (
+    <View style={styles.container}>
+      {url && <Text style={{ fontSize: 30 }}>{url}</Text>}
+      <Text style={{ color: "#313639", fontFamily: "Inter-Black" }}>
+        Firebase Demo
+      </Text>
+      {session ? (
+        <View>
+          <Text style={{ color: "#313639", fontFamily: "Inter-Black" }}>
+            Firebase User ID: {session.uid}
+          </Text>
+          <Button
+            title="Sign Out"
+            onPress={() =>
+              auth()
+                .signOut()
+                .catch((error) =>
+                  Alert.alert("Authentication Error", error.message)
+                )
+            }
+          />
+        </View>
+      ) : (
+        <Button
+          title="Sign In"
+          onPress={() =>
+            auth()
+              .signInAnonymously()
+              .catch((error) =>
+                Alert.alert("Authentication Error", error.message)
+              )
+          }
+        />
+      )}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FAF8F5",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
