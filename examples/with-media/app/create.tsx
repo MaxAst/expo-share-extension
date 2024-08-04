b535958b7668e388d3eacb8910c99224a5fc0c7b
Max Ast <maxast31@gmail.com>
2024-08-04 15:44:10 +0200

    do not use expo-file-system in with-media example

--- examples/with-media/app/create.tsx
+++ examples/with-media/app/create.tsx
@@ -1,44 +1,14 @@
-import * as FileSystem from "expo-file-system";
 import { useLocalSearchParams } from "expo-router";
 import { clearAppGroupContainer } from "expo-share-extension";
-import { useCallback, useEffect } from "react";
+import { useEffect } from "react";
 import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function Create() {
   const { videoUrl } = useLocalSearchParams();
 
-  const checkSharedDataDirectory = useCallback(async () => {
-    if (!videoUrl || typeof videoUrl !== "string") return;
-    const regex = /(.*\/AppGroup\/)/;
-    const match = videoUrl.match(regex);
-
-    const appGroupPath = match ? match[1] : null;
-
-    if (!appGroupPath) {
-      throw new Error(
-        "Failed to parse AppGroup path from iOS share extension video url"
-      );
-    }
-
-    try {
-      const files = await FileSystem.readDirectoryAsync(appGroupPath);
-      console.log(
-        "Includes sharedData directory?",
-        files.includes("sharedData")
-      );
-      if (files.includes("sharedData")) {
-        const sharedDataFiles = await FileSystem.readDirectoryAsync(
-          `${appGroupPath}sharedData`
-        );
-        console.log(sharedDataFiles);
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  }, [videoUrl]);
-
   useEffect(() => {
-    checkSharedDataDirectory();
+    // use sth. like expo-video-metadata to process video file
+    console.log(videoUrl)
   }, [videoUrl]);
 
   const handleClearAppGroupContainer = async () => {
@@ -47,8 +17,6 @@ export default function Create() {
     } catch (error) {
       console.error(error);
     }
-
-    await checkSharedDataDirectory();
   };
 
   return (
