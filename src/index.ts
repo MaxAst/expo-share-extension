b7f7614fa160cb9f0a2e8a5c74e416cc6e02bffb
Max Ast <maxast31@gmail.com>
2024-12-27 17:55:08 +0100

    add metro helper function

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { getDefaultConfig } from "expo/metro-config";
+
 import ExpoShareExtensionModule from "./ExpoShareExtensionModule";
 
 export function close(): void {
@@ -28,4 +30,37 @@ export type InitialProps = {
   text?: string;
   url?: string;
   preprocessingResults?: unknown;
+};
+
+export const withShareExtension = (
+  config: ReturnType<typeof getDefaultConfig>,
+) => {
+  if (!config.resolver) {
+    throw new Error("config.resolver is not defined");
+  }
+
+  config.resolver.sourceExts = [
+    ...(config.resolver?.sourceExts ?? []),
+    "share.js",
+  ];
+
+  if (!config.server) {
+    throw new Error("config.server is not defined");
+  }
+
+  const originalRewriteRequestUrl =
+    config.server?.rewriteRequestUrl || ((url) => url);
+
+  config.server.rewriteRequestUrl = (url) => {
+    const isShareExtension = url.includes("shareExtension=true");
+    const rewrittenUrl = originalRewriteRequestUrl(url);
+
+    if (isShareExtension) {
+      return rewrittenUrl.replace("index.bundle", "index.share.bundle");
+    }
+
+    return rewrittenUrl;
+  };
+
+  return config;
 };
